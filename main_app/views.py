9219907a4fb99c09aa339c040da246d190fa6f2e
James Palumbo <jamesmpalumbo1@gmail.com>
2025-05-27 18:20:47 -0400

    added full CRUD functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,49 @@
 from django.shortcuts import render
-# Import HttpResponse to send text-based responses
 from django.http import HttpResponse
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse('<h1>Hello World! ᓚᘏᗢ</h1>')
 
-# Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return render(request, 'home.html')
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
 
 def about(request):
     return render(request, 'about.html')
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+def home(request):
+    return render(request, 'home.html')
 
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
+
+# cats = [
+#     Cat('Sarabi', 'lion', 'hunts and sleeps.', 5),
+#     Cat('Oliver', 'Orange', 'Was cool with some dogs', 1),
+#     Cat('Thomas Omalley', 'Orange Stray', 'felt everybody wanted to be a cat', 3 ),
+#     Cat('Simba', 'lion cub', 'peer pressured into eating worms', 2),
+# ]
 
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all() 
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
