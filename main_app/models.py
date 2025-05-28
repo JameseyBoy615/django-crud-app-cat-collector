80cd7767cb355e4ab3d53f3bd6efa1dc38067101
James Palumbo <jamesmpalumbo1@gmail.com>
2025-05-27 20:47:21 -0400

    added feedings model and functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -16,3 +23,17 @@ class Cat(models.Model):
         return reverse('cat-detail', kwargs={'cat_id': self.id})
 
     
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    # Define the default order of feedings
+    class Meta:
+        ordering = ['-date']  # This line makes the newest feedings appear first
+
